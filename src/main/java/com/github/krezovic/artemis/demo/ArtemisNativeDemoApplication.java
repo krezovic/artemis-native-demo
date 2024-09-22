8b74df281cd9b9aff7830dc736d6557669ac35ad
Armin Krezović <krezovic.armin@gmail.com>
2024-09-22 15:59:25 +0200

    Initial commit

---
+++ src/main/java/com/github/krezovic/artemis/demo/ArtemisNativeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.krezovic.artemis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemisNativeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtemisNativeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/krezovic/artemis/demo/ArtemisNativeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.krezovic.artemis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtemisNativeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
